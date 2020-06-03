3d48682786598e75ef846789772c2a0610aac522
Isaac Zhang <zhangpan0614@126.com>
2020-06-03 22:49:52 +0800

    spring bean registry Using API BeanDefinitionRegistry & BeanDefinitionReaderUtils

--- learning-spring/spring-beans-demo/src/main/java/com/liferunner/learning/spring/beans/BeanDefinitionRegistryDemo.java
+++ learning-spring/spring-beans-demo/src/main/java/com/liferunner/learning/spring/beans/BeanDefinitionRegistryDemo.java
@@ -1,11 +1,15 @@
 package com.liferunner.learning.spring.beans;
 
 import com.liferunner.learning.spring.pojo.Person;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
@@ -30,6 +34,30 @@ public class BeanDefinitionRegistryDemo {
         registryBeanUsingAnnotation();
         // 3. @Import 注册 bean
         registryBeanUsingImport();
+        // 4. 使用 API 注册 bean(带 beanName)
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        registryBeanUsingAPI(applicationContext, "isa-bean", Person.class);
+        // 5. 使用 API 注册 bean(不带 beanName)
+        registryBeanUsingAPI(applicationContext, null, Person.class);
+        applicationContext.refresh();
+        // 获取注册的 beans
+        Map<String, Person> personMap = applicationContext.getBeansOfType(Person.class);
+        System.out.println("come from main..." + personMap);
+        applicationContext.close();
+    }
+
+    private static void registryBeanUsingAPI(BeanDefinitionRegistry registry, String beanName, Class<?> beanClazz) {
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(beanClazz)
+                .addPropertyValue("id", 3L)
+                .addPropertyValue("name", "isa")
+                .addPropertyValue("age", 18);
+
+        if (StringUtils.hasText(beanName)) {
+            registry.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
+        } else {
+            BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinitionBuilder.getBeanDefinition(), registry);
+        }
     }
 
     private static void registryBeanUsingImport() {
@@ -39,21 +67,21 @@ public class BeanDefinitionRegistryDemo {
         // 启动上下文
         applicationContext.refresh();
         // 获取注册的 beans
-        Map<String,Person> personMap = applicationContext.getBeansOfType(Person.class);
-        System.out.println("come from import..."+personMap);
+        Map<String, Person> personMap = applicationContext.getBeansOfType(Person.class);
+        System.out.println("come from import..." + personMap);
         // 关闭应用上下文
         applicationContext.close();
     }
 
 
-    public static void registryBeanUsingXML(){
+    public static void registryBeanUsingXML() {
         // 创建英文上下文
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/spring/Bean-Definition-Context.xml");
         // 启动上下文
         applicationContext.refresh();
         // 获取注册的 beans
-        Map<String,Person> personMap = applicationContext.getBeansOfType(Person.class);
-        System.out.println("come from xml..."+personMap);
+        Map<String, Person> personMap = applicationContext.getBeansOfType(Person.class);
+        System.out.println("come from xml..." + personMap);
         // 关闭应用上下文
         applicationContext.close();
     }
@@ -64,17 +92,17 @@ public class BeanDefinitionRegistryDemo {
         // 启动上下文
         applicationContext.refresh();
         // 获取注册的 beans
-        Map<String,Person> personMap = applicationContext.getBeansOfType(Person.class);
-        System.out.println("come from annotation..."+personMap);
+        Map<String, Person> personMap = applicationContext.getBeansOfType(Person.class);
+        System.out.println("come from annotation..." + personMap);
         // 关闭应用上下文
         applicationContext.close();
     }
 
     @Component
-    public static class AnnotationBeansConfigurationClazz{
+    public static class AnnotationBeansConfigurationClazz {
 
         @Bean(name = {"annotated-person,isa,dy"})
-        public Person person(){
+        public Person person() {
             Person person = new Person();
             person.setAge(18);
             person.setId(2L);
