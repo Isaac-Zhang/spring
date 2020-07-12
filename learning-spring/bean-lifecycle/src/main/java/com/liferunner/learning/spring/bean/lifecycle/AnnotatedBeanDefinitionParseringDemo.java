342078e15876675a7f28c7746bd41068b7213d6b
Isaac Zhang <zhangpan0614@126.com>
2020-07-12 15:18:38 +0800

    使用 AnnotatedBeanDefinitionReader 创建 Bean

---
+++ learning-spring/bean-lifecycle/src/main/java/com/liferunner/learning/spring/bean/lifecycle/AnnotatedBeanDefinitionParseringDemo.java
@@ -0,0 +1,36 @@
+package com.liferunner.learning.spring.bean.lifecycle;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+
+/**
+ * 注解 {@link BeanDefinition} 注册示例
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
+ * @since 2020/7/12
+ **/
+public class AnnotatedBeanDefinitionParseringDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        AnnotatedBeanDefinitionReader annotatedBeanDefinitionReader = new AnnotatedBeanDefinitionReader(beanFactory);
+
+        int beforeCount = beanFactory.getBeanDefinitionCount();
+        BeanNameGenerator beanNameGenerator = (definition, registry) -> "Bean-isaacGenerateName-";
+        annotatedBeanDefinitionReader.setBeanNameGenerator(beanNameGenerator);
+        // 注册 BeanDefinition
+        annotatedBeanDefinitionReader.register(AnnotatedBeanDefinitionParseringDemo.class);
+        int afterCount = beanFactory.getBeanDefinitionCount();
+        System.out.println("当前注入的 BeanDefinition 数量：" + (afterCount - beforeCount));
+        // 表明注册的 Bean 默认名称 为当前类的 首字母小写
+//        System.out.println(beanFactory.getBean("annotatedBeanDefinitionParseringDemo",AnnotatedBeanDefinitionParseringDemo.class));
+        System.out.println(beanFactory.getBean(AnnotatedBeanDefinitionParseringDemo.class));
+
+
+        System.out.println(beanFactory.getBeanNamesForType(AnnotatedBeanDefinitionParseringDemo.class));
+    }
+}
