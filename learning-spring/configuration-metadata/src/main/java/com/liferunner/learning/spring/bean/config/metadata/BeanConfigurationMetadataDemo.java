4fa9c3520587f45b902ff448a7c163e6170536dd
Isaac Zhang <zhangpan0614@126.com>
2020-07-16 22:39:22 +0800

    Bean AttributeAccessor

---
+++ learning-spring/configuration-metadata/src/main/java/com/liferunner/learning/spring/bean/config/metadata/BeanConfigurationMetadataDemo.java
@@ -0,0 +1,44 @@
+package com.liferunner.learning.spring.bean.config.metadata;
+
+import com.liferunner.learning.spring.pojo.Person;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * Bean 配置元信息示例
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
+ * @since 2020/7/16
+ **/
+public class BeanConfigurationMetadataDemo {
+
+    public static void main(String[] args) {
+//        BeanDefinition beanDefinition = new GenericBeanDefinition();
+//        beanDefinition.setAttribute("name","张盼");
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(Person.class);
+        beanDefinitionBuilder.addPropertyValue("name", "isaac");
+        AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+        beanDefinition.setAttribute("name", "张盼");
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.addBeanPostProcessor(new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (ObjectUtils.nullSafeEquals("person", beanName)
+                        && bean.getClass().equals(Person.class)) {
+                    BeanDefinition bd = beanFactory.getBeanDefinition(beanName);
+                    System.out.println(bd.getAttribute("name"));
+                }
+                return bean;
+            }
+        });
+
+        beanFactory.registerBeanDefinition("person", beanDefinition);
+
+        System.out.println(beanFactory.getBean("person", Person.class));
+    }
+}
