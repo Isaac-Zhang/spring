7d47a1436cc21bd11b388e671dbff59b81916dab
Isaac Zhang <zhangpan0614@126.com>
2020-07-19 22:40:52 +0800

    PathMatchingResourcePatternResolver 下自定义 PathMatcher

--- learning-spring/spring-resources/src/main/java/com/liferunner/learning/spring/resources/CustomResourcePatternResolverDemo.java
+++ learning-spring/spring-resources/src/main/java/com/liferunner/learning/spring/resources/CustomResourcePatternResolverDemo.java
@@ -5,8 +5,11 @@ import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.PathMatcher;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
@@ -27,8 +30,48 @@ public class CustomResourcePatternResolverDemo {
                 new FileSystemResourceLoader()
         );
 
+        // 应用自定义 PathMatcher 选择器
+        patternResolver.setPathMatcher(new MyJavaFilePathMatcher());
         Resource[] resources = patternResolver.getResources(locationPattern);
 
         Stream.of(resources).map(ResourceUtils::getContext).forEach(System.out::println);
+    }
+
+    static class MyJavaFilePathMatcher implements PathMatcher {
+
+        @Override
+        public boolean isPattern(String path) {
+            return path.endsWith(".java");
+        }
+
+        @Override
+        public boolean match(String pattern, String path) {
+            return path.endsWith(".java");
+        }
+
+        @Override
+        public boolean matchStart(String pattern, String path) {
+            return false;
+        }
+
+        @Override
+        public String extractPathWithinPattern(String pattern, String path) {
+            return null;
+        }
+
+        @Override
+        public Map<String, String> extractUriTemplateVariables(String pattern, String path) {
+            return null;
+        }
+
+        @Override
+        public Comparator<String> getPatternComparator(String path) {
+            return null;
+        }
+
+        @Override
+        public String combine(String pattern1, String pattern2) {
+            return null;
+        }
     }
 }
